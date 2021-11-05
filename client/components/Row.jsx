48ccf7a8fc848fe9b332b15799ab598358661631
Adam Berri <78669179+AdamBerri@users.noreply.github.com>
2021-11-05 17:09:14 -0400

    Add files via upload

---
+++ client/components/Row.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import SoundCell from './SoundCell.jsx';
+//import CellOperation from './CellOperation';
+
+
+const Row = (props) => {
+
+
+  makeElements();
+  //console.log(cellElements)
+return (
+  <div>
+    <h1>this is a Row</h1>
+  </div>
+)
+}
+
+export default Row;
---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App.jsx';
+
+
+import styles from './stylesheets/__styles.scss';
+
+
+render(
+  <App />,
+  document.getElementById('root')
+);
