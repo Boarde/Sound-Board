6133be07200bad34f396d3c7e6bffc79cd37b746
edwardkwon95 <66803062+edwardkwon95@users.noreply.github.com>
2021-11-06 10:27:52 -0400

    removed comments from App component

--- client/App.jsx
+++ client/App.jsx
@@ -8,24 +8,6 @@ function App (){
   const [allSounds, setAllSounds] = useState([]);
   // useEffect is like componentDidMount componentDidUnmount
   useEffect(() => {
-      //fetch request to backend
-      // async () => {
-      //   const sounds = await fetch('/all', {
-      //     method: 'GET',
-      //     headers: {
-      //       'Content-Type': 'application/json',
-      //     }
-      //   })
-      //     .then(res => res.json())
-      //     .then(data => {
-      //       console.log('This is our data =>', data.rows);
-      //     })
-      //     .catch(err => {
-      //       console.log("Error fetching request from back end");
-      //     });
-      //     console.log('ASYNC FETCH: ', sounds)
-      //     setAllSounds(sounds);
-      // }
       fetch('/all', {
         method: 'GET',
         headers: {
