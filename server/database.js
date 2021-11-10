69433b504881e6b4c4915a150c5a60f655eb8210
Jennifer Chau <jenniferchau@Jennifers-iMac.local>
2021-11-09 20:14:10 -0500

    comment out URI

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const PG_URI = 'postgres://fbhhtjnu:7nX6UUwB6gHXtEweTcwOjYXX2bm4ZtEW@fanny.db.elephantsql.com/fbhhtjnu';
+// const PG_URI = 'postgres://fbhhtjnu:7nX6UUwB6gHXtEweTcwOjYXX2bm4ZtEW@fanny.db.elephantsql.com/fbhhtjnu';
 
 const pool = new Pool({
   connectionString: PG_URI
