34d4386c14e0fbc121cedc8b6a118b588e7c8883
Chang Cai <ccai89@gmail.com>
2021-11-09 21:59:52 -0500

    Added comments, change some conditional rendering
    Removed superfluous states, and added defaults state.  Changed interface positioning.

--- client/App.jsx
+++ client/App.jsx
@@ -1,28 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import Board from './components/Board.jsx';
+<<<<<<< HEAD
+import Playlist_Selector from './components/Preset_Selector.jsx';
+import Customizer from './components/Customizer.jsx';
+=======
 import Preset_Selector from './components/Preset_Selector.jsx';
 import Customizer from './components/Customizer.jsx'
+>>>>>>> upstream/dev
 
 import './stylesheets/styles.scss';
 
 
 function App() {
-  // STATE FOR ALL SOUNDS
+  // holds all the sounds in a playlist
   const [allSounds, setAllSounds] = useState([]);
-  // STATE FOR PRESETS
-  const [preset, setPreset] = useState('gaffes');
+  // holds all the playlists
+  const [preset, setPreset] = useState('');
   // STATE FOR CONDITIONAL RENDERING BOARD VS MENU
   const [menuStatus, setMenuStatus] = useState(false);
-  // STATE FOR DEFAULT PRESETS ON PAGE LOAD
-  const [defaultPresets, setDefaultPresets] = useState([]);
-  // STATE FOR SHOWING LOGIN FORM 
+  // holds list of playlists 
+  const [playlists, setPlaylists] = useState([]);
+  // conditional for showing login form
   const [showLogin, setShowLogin] = useState(false);
   // Saving username and password in States
   const [currUser, setCurrUser] = useState(null);
   const [password, setPassword] = useState('');
+  const [loginStatus, setStatus] = useState(false);
 
-  const logOut = () => {
-    setCurrUser(null);
+  // account settings - sign up, log in, log out
+  const postSignUp = () => {
+    fetch('/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ allInfo: { username: currUser, password: password } })
+      //const { username, password } = req.body.allInfo;
+    })
+      .then(res => {
+        setCurrUser(currUser);
+      })
+      .catch(err => {
+        console.log('Error sign up', err);
+      });
   };
 
   const postLogIn = () => {
@@ -37,61 +57,65 @@ function App() {
       .then(data => {
         setCurrUser(currUser);
         setAllSounds(data);
-        setDefaultPresets(Object.keys(data));
+        setShowLogin(false);
+        setStatus(true);
+        setPlaylists(Object.keys(data));
       })
       .catch(err => {
-        console.log("Error logging in user", err);
+        console.log('Error logging in user', err);
       });
   };
 
-  const postSignUp = () => {
-    fetch('/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ allInfo: { username: currUser, password: password } })
-      //const { username, password } = req.body.allInfo;
-    })
-      .then(res => {
-        setCurrUser(currUser);
-      })
-      .catch(err => {
-        console.log("Error sign up", err);
-      });
+  const logOut = () => {
+    setCurrUser(null);
+    setPreset('');
+    setAllSounds([]);
+    setPlaylists['default'];
+    setStatus(false);
+    setMenuStatus(false);
   };
+
+  // render login form
+  const loginForm =
+    <div key="login" className="login-wrapper">
+      <form>
+        <div id="login-input">Username: <input onChange={e => setCurrUser(e.target.value)} type="text" required></input></div>
+        <div id="login-input">Password: <input onChange={e => setPassword(e.target.value)} type="password" required></input></div>
+        <div className="outer">
+          <button className="login-button-click" onClick={e => { e.preventDefault(); postLogIn(); }}>Log In</button>
+          <button className="login-button-click" onClick={postSignUp}>Sign Up</button>
+        </div>
+      </form>
+    </div>;
+
+
 
   return (
     //load user settings and render the board
     <div className="app-wrapper">
-      {/* DISPLAYS GEAR FOR SETTINGS */}
-      <button className="presetSettings" onClick={() => setMenuStatus(!menuStatus)}></button>
-      {!currUser && <button id="login-form" onClick={() => setShowLogin(!showLogin)}></button>}
+      {/* displays log in icon while user is not logged in */}
+      {!loginStatus && <button id="login-form" onClick={() => setShowLogin(!showLogin)}></button>}
 
-      {/* LOGIN BLOCK */}
-      {(showLogin && !currUser) &&
-        <div className="login-wrapper">
-          <form>
-            <div id="username-input" style={{ color: 'white' }}>Username: <input onChange={e => setCurrUser(e.target.value)} type="text" required></input></div>
-            <div id="login-input" style={{ color: 'white' }}>Password: <input onChange={e => setPassword(e.target.value)} type="password" required></input></div>
-            <div className="outer">
-              <button className="login-button-click" onClick={e => {e.preventDefault();postLogIn();}}>Log In</button>
-              <button className="login-button-click" onClick={postSignUp}>Sign Up</button>
-            </div>
-          </form>
-        </div>
-      }
+      {/* displays login form when button is pressed*/}
+      {showLogin && loginForm}
 
-      {/* LOG OUT BUTTON */}
-      {currUser && <button id="log-out-button" onClick={logOut}></button>}
-
+      {/* displays gear for the settings when logged in */}
+      {loginStatus && <button className="presetSettings" onClick={() => setMenuStatus(!menuStatus)}></button>}
+      
+      {/* displays customizer page when button is pressed */}
       {menuStatus && <Customizer currUser={currUser} setMenuStatus={setMenuStatus} allSounds={allSounds} />}
-      {(!currUser && menuStatus) || <Preset_Selector defaultPresets={defaultPresets} setPreset={setPreset} />}
+      
+
+      {/* displays playlist slector when logged in */}
+      {loginStatus && <Playlist_Selector playlists={playlists} setPreset={setPreset} />}
+      
+      {/* most important item - the sound board */}
       {menuStatus || <Board preset={preset} allSounds={allSounds} />}
+      
+      {/* displays logout icon when logged in */}
+      {loginStatus && <button id="logout-button" onClick={logOut}></button>}
     </div>
-  )
-
+  );
 }
-
 
 export default App;
