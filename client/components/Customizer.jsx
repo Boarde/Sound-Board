f918188c7a2aa8fcd5284a1d017c51f324eca1b5
ccai89 <ccai89@gmail.com>
2021-11-10 16:29:54 -0500

    merged methods to generate drop downs

--- client/components/Customizer.jsx
+++ client/components/Customizer.jsx
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
 const Customizer = (props) => {
-  // where we create a new preset
   const [newPreset, setNewPreset] = useState([]);
   const [presetName, setPresetName] = useState('');
 
   const soundList = props.allSounds;
-  const preset = props.preset;
 
-  // useEffect(() => {
-  //   const defaultPreset = [];
-  //   for (let i = 0; i < 12; i++) {
-  //     defaultPreset.push(currentSounds[0]);
-  //   }
-  //   setNewPreset(defaultPreset);
-  // }, []);
+  useEffect(() => {
+    const defaultPreset = [];
+    for (let i = 0; i < 12; i++) {
+      defaultPreset.push(currentSounds[0]);
+    }
+    setNewPreset(defaultPreset);
+  }, []);
+
+  let databaseEntry = '';
 
   const currentSounds = [];
   const soundsArray = () => {
@@ -22,40 +22,48 @@ const Customizer = (props) => {
       for (let i = 0; i < soundList[element].length; i++) {
         if (!currentSounds.includes(soundList[element][i].name)) {
           currentSounds.push(soundList[element][i].name);
+
         }
       }
     }
     );
   };
 
-  let databaseEntry = '';
-
   soundsArray();
-  const formElements = currentSounds.map((element, i) => (
-    <option key={`${i}`} value={element}> {element} </option>
-  ));
+
+
+
 
   // POST FETCH REQUEST
   //Instead we should submit an array with the ...Object.values(newPreset) AND the links right here) 
   const addPreset = () => {
+    //console.log(props.currUser);
     props.setMenuStatus(false);
-    databaseEntry = [presetName, ...Object.values(newPreset)];
+    databaseEntry = [presetName, ...Object.values(newPreset), props.currUser];
+    console.log('databaseentry', databaseEntry);
     fetch('/savePreset', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ newPreset: [presetName, ...Object.values(newPreset), props.currUser/*, username */] })
-      //body has to be in this format { newPreset : ['Connor','charmander','whip','two_hours_later','xylophone','marimba','zither','gta','what_are_those','recorder','vulpix','fbi','ash_boogy'];
+      //body has to be in this format { newPreset : ['Connor','charmander','whip','two_hours_later','xylophone','marimba','zither','gta','what_are_those','recorder','vulpix','fbi','ash_boogy', USERNAME];
 
       // }
     })
-      .then(res => res)
+      .then(res => res.json())
+      .then (data => {
+        console.log(data);
+        props.setAllSounds(data);
+        props.setPlaylists(Object.keys(data));
+      })
       .catch(err => {
         console.log('ERROR CREATING NEW PRESET');
         return err;
       });
+
   };
+
 
   function setPlaylist(i, e) {
     const selectedPreset = JSON.parse(JSON.stringify(newPreset));
@@ -63,16 +71,15 @@ const Customizer = (props) => {
     setNewPreset(selectedPreset);
   }
 
-  // generates 12 dropdowns 
   const presetOptions = [];
   for (let i = 0; i < 12; i++) {
     presetOptions.push(
-      <div className="customizer-dropdown-wrapper"><div style={{color:"white"}}>{soundList[preset][i].link}</div>
+      <div className="customizer-dropdown-wrapper">
         <select onChange={e => setPlaylist(i, e)} id={`${i}dropdown`} name="soundClips">
-          {formElements}
+          {currentSounds.map((element, i) => (<option key={`${i}`} value={element}> {element} </option>))}
         </select>
       </div>
-    );
+    )
   }
 
   return (
@@ -86,12 +93,15 @@ const Customizer = (props) => {
           <input onChange={(e) => setPresetName(e.target.value)} id="preset-name" type="text" required></input>
           <input type="submit"></input>
         </div>
+
         <div className="preset-wrapper">
           {presetOptions}
         </div>
+
       </form>
     </div>
-  );
-};
+
+  )
+}
 
 export default Customizer;
