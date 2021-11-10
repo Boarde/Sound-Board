53ea40dfa3db85466473d37f707da382cbd61f31
Adam Berri <adamberri@adams-mac-mini.lan>
2021-11-10 10:07:32 -0500

    yes

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -1,31 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import SoundButton from './SoundButton.jsx';
 
+
 const Board = (props) => {
-  const preset = props.preset;
+  const allSoundButtons = [];
+  const defaultPresets = props.defaultPresets;
+  console.log('this console.logs after an option is chosen within the dropdown', defaultPresets)
+  // console.log('PRESET CHANGED', preset)
+  //because of an issue where props.allSounds is rendering after the sounds constant is initialized
   //we are initializing it to an empty array in case
-  const sounds = props.allSounds[preset] || [];
-  // defaults any empty slots to 'boop'
+  console.log('props---->', props)
+  console.log('before props console log---->', props.allSounds)
+  const sounds = props.allSounds || [];
+  // console.log('THIS IS THE SOUND FROM PRESET, ', sounds);
   const defaultSound = 'https://www.myinstants.com/media/sounds/overwatch-boop-sombra.mp3';
-    
-  // 
-  const renderBoard = () => {
-    const soundboard = [];
+  console.log('after props console log---->', props.allSounds[props.preset])
+  // CONDITIONAL LOGIC TO CHANGE BUTTONS RENDERED DEPENDING ON PRESET
+  // PRESET CHANGES HAPPEN IN SETTINGS.JSX
+  
+  // TESTING BUTTON RENDERING
+  const renderButtons = () => {
     for (let i = 0; i < 12; i++) {
       if (sounds[i] !== undefined) {
-        soundboard.push(<SoundButton id={i} key={i} sound={ sounds[i].link } />);
+        allSoundButtons.push(<SoundButton id={i} key={i} sound={ sounds[i].link } />)
       } else {
-        soundboard.push(<SoundButton id={i} key={i} sound={ defaultSound } />);
+        allSoundButtons.push(<SoundButton id={i} key={i} sound={ defaultSound } />)
       }
     }
-    return soundboard;
-  };
+  }
+  renderButtons();
  
   return (
-    <div className="soundboard">
-      { renderBoard() }
+    <div className="board">
+      { allSoundButtons }
     </div>
-  );
-};
+  )
+}
 
 export default Board;
