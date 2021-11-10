19686768eb798d5b3376cdde9fa95474afdd24bc
Chang Cai <ccai89@gmail.com>
2021-11-09 21:55:49 -0500

    Refactored to decrease redundancy and added comments

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -3,27 +3,26 @@ import SoundButton from './SoundButton.jsx';
 
 const Board = (props) => {
   const preset = props.preset;
-  //we are initializing it to an empty array in case
-  const sounds = props.allSounds[preset] || [];
-  // defaults any empty slots to 'boop'
-  const defaultSound = 'https://www.myinstants.com/media/sounds/overwatch-boop-sombra.mp3';
-    
-  // 
-  const renderBoard = () => {
-    const soundboard = [];
-    for (let i = 0; i < 12; i++) {
-      if (sounds[i] !== undefined) {
-        soundboard.push(<SoundButton id={i} key={i} sound={ sounds[i].link } />);
-      } else {
-        soundboard.push(<SoundButton id={i} key={i} sound={ defaultSound } />);
-      }
-    }
-    return soundboard;
-  };
- 
+  // default sounds to populate board when user does not have an account or is not logged in
+  const sounds = props.allSounds[preset] || [
+    {link: 'https://www.myinstants.com/media/sounds/overwatch-boop-sombra.mp3' },
+    {link: 'https://www.pokezorworld.com/anime/wav/pikach.wav' },
+    {link: 'https://quicksounds.com/uploads/tracks/155995082_386652093_1130616536.mp3' },
+    {link: 'https://www.pokezorworld.com/anime/wav/whosthatpokemon.wav' },
+    {link: 'https://quicksounds.com/uploads/tracks/1276474019_2059256748_1376796276.mp3' },
+    {link: 'https://www.imit.org.uk/sound-clips/Whip.mp3' },
+    {link: 'https://quicksounds.com/uploads/tracks/684591523_732065353_968365190.mp3' },
+    {link: 'https://www.pokezorworld.com/anime/wav/charmander.wav' },
+    {link: 'https://www.pokezorworld.com/anime/wav/machoke.wav' },
+    {link: 'https://www.imit.org.uk/sound-clips/Organ.mp3' },
+    {link: 'https://www.pokezorworld.com/anime/wav/bulbasaur.wav' },
+    {link: 'https://www.pokezorworld.com/anime/wav/meowth.wav' }
+  ];
+
+  // rendering entire board
   return (
     <div className="soundboard">
-      { renderBoard() }
+      {sounds.map((el, i) => <SoundButton id={i} key={i} sound={ sounds[i].link } />)}
     </div>
   );
 };
